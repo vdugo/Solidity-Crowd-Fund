91e7a46bae76e6b0412b216a5957228ec2beb15b
Vincent <v3rdugo@yahoo.com>
2022-08-12 16:03:01 -0700

    set up Campaign struct and state variables

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -16,5 +16,28 @@ import "./IERC20.sol";
  */
 contract FundMe
 {
-
+    /**
+     * @dev Stores the data of each campaign. Notice the use of uint32 instead
+     * of uint256, uint32 can hold times up to about 100 years from now in Unix time.
+     * We don't need more bits than 32.
+     */
+    struct Campaign
+    {
+        address creator; /// the address of the creator of a campaign
+        uint256 goal; /// the target amount of tokens that the creator wishes to raise from donors.
+        uint32 startAt; /// the starting time of the campaign in Unix time
+        uint32 endAt; /// the ending time of the campaign in Unix time
+        bool claimed; /// boolean to check if the creator has claimed the tokens
+    }
+    /**
+     * State Variables
+     */
+    /// token will only be set once on creation
+    IERC20 public immutable token; 
+    /// counter so that we can generate a unique id for every campaign
+    uint256 public count; 
+    /// mapping from id of campaign to the campaign
+    mapping(uint256 => Campaign) public campaigns; 
+    /// mapping from campaign id to another mapping from donor address to amount donated
+    mapping(uint256 => mapping(address => uint256)) public pledgedAmount;
 }
