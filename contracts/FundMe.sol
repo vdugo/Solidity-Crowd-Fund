6ab2df544d35597958f33320cb3d10e8b06e2296
Vincent <v3rdugo@yahoo.com>
2022-08-15 19:07:14 -0700

    use OpenZeppelin ERC20.sol instead

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "./IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @dev A crowd funding contract for ERC-20 tokens.
