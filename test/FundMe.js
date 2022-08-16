bc1c19e197b521fdf1d411e0f92dccf8cad90064
Vincent <v3rdugo@yahoo.com>
2022-08-15 22:39:12 -0700

    all tests for launch function passing

--- test/FundMe.js
+++ test/FundMe.js
@@ -1,11 +1,22 @@
 const { ethers, network } = require('hardhat')
 const { assert, expect } = require('chai')
 
+getCurrentTime = async () =>
+{
+    let currentBlock = await ethers.provider.getBlock()
+    return currentBlock.timestamp
+}
+
 describe('FundMe Unit Tests', async () =>
 {
     
     const TOKEN_NAME = 'Vincent'
     const TOKEN_SYMBOL = 'VIN'
+
+    // 1000 of any ERC-20 token with default 18 decimal places
+    const GOAL = ethers.utils.parseUnits('1000')
+
+    const NINETY_DAYS_IN_SECONDS = 90 * 24 * 60 * 60
 
     const DONATION_AMOUNT = ethers.utils.parseEther('0.1')
 
@@ -13,8 +24,12 @@ describe('FundMe Unit Tests', async () =>
 
     let deployer, donor
 
+    let testStartBlock
+
     beforeEach(async () =>
     {
+        testStartBlock = await ethers.provider.getBlock()
+
         const accounts = await ethers.getSigners()
         deployer = accounts[0]
         donor = accounts[1]
@@ -31,7 +46,60 @@ describe('FundMe Unit Tests', async () =>
         it('initializes the token state variable to the ERC20 token', async () =>
         {
             const tokenAddress = await fundMe.token()
-            assert(erc20.address === tokenAddress, 'token not initialized properly to FundMe')
+            assert(erc20.address === tokenAddress, 'token not initialized properly to Fund')
+        })
+    })
+
+    describe('launch', async () =>
+    {
+        it('rejects an invalid starting time', async () =>
+        {
+            let currentTime = await getCurrentTime()
+            await expect(fundMe.launch(GOAL, currentTime - 1, currentTime + 10000)).to.be.reverted
+        })
+
+        it('rejects an invalid ending time', async () =>
+        {
+            let currentTime = await getCurrentTime()
+            await expect(fundMe.launch(GOAL, currentTime + 10, currentTime - 10)).to.be.reverted
+        })
+
+        it('enforces time limit of 90 days on how long the campaign can last', async () =>
+        {
+            let currentTime = await getCurrentTime()
+            await expect(fundMe.launch(GOAL, currentTime + 10, currentTime + NINETY_DAYS_IN_SECONDS + 10)).to.be.reverted
+        })
+
+        it('increments the count state variable', async () =>
+        {
+            let currentTime = await getCurrentTime()
+            await fundMe.launch(GOAL, currentTime + 10, currentTime + 10000)
+
+            let count = await fundMe.count()
+            assert(count.toString() === '1', 'count.toString() !== 1')
+        })
+
+        it('stores the campaign struct in the campaigns mapping', async () =>
+        {
+            let currentTime = await getCurrentTime()
+            await fundMe.launch(GOAL, currentTime + 10, currentTime + 10000)
+
+            let count = await fundMe.count()
+
+            const campaign = await fundMe.campaigns(count)
+
+            assert(campaign.creator.toString() === deployer.address)
+            assert(campaign.goal.toString() === GOAL.toString())
+            assert(campaign.pledged.toString() === '0')
+            assert(campaign.startAt.toString() === (currentTime + 10).toString())
+            assert(campaign.endAt.toString() === (currentTime + 10000).toString())
+            assert(campaign.claimed === false)
+        })
+
+        it('emits a Launch event', async () =>
+        {
+            let currentTime = await getCurrentTime()
+            expect(await fundMe.launch(GOAL, currentTime + 10, currentTime + 10000)).to.emit('Launch')
         })
     })
 })
